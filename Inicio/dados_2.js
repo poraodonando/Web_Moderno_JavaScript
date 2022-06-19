1f506e3bf1c392d9062322bd074f8690292c2293
ProfNando <mestre.physics@gmail.com>
2022-06-19 19:37:52 -0300

    dados_2

---
+++ Inicio/dados_2.js
@@ -0,0 +1,13 @@
+var preco = 19.9;
+let = desconto = 0.4 ; 
+//VAR(ou LET) - define uma introdução de uma variavel
+//IDENTIFICADOR - descreve o nome de uma variavel
+//VALOR - atribui um determinado valor ao identificador
+
+console.log(19.9 * 0.6);
+console.log(preco * (1- desconto));
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+ console.log("Produto: "+ nome+ ", Categoria: "+ categoria);
+
