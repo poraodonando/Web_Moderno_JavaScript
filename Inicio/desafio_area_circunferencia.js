586d6b1bcc83fa33a4fbb7a2eebd8355de3958d6
ProfNando <mestre.physics@gmail.com>
2022-06-19 20:21:06 -0300

    desafio area

---
+++ Inicio/desafio_area_circunferencia.js
@@ -0,0 +1,10 @@
+// Area = PI * raio * raio
+
+const PI = 3.1415;  // Algo que nao mmuda, colocamos em letra maiuscula
+
+let raio = 10;
+let Area;
+
+Area = PI * raio * raio
+
+console.log("A area da circunferencia e "+ Area + " m2. " )
