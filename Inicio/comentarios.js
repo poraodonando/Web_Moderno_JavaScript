81522534d20a64c90134d870e3fc6c2ddbe23583
ProfNando <mestre.physics@gmail.com>
2022-06-19 16:59:21 -0300

    comentatios

---
+++ Inicio/comentarios.js
@@ -0,0 +1,12 @@
+// Serve para documentar.
+//Desabilitar alguma linha de código.
+
+
+/**
+ * 
+ * Varios Comentarios
+ * em uma parte do código
+ * 
+ */
+
+// se colocar /** */ serão gerados asteriscos automaticamente
