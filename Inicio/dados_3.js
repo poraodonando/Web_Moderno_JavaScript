b89a7a89ba215a25bbe22b0764957116de90114c
ProfNando <mestre.physics@gmail.com>
2022-06-19 19:47:29 -0300

    dados_3

---
+++ Inicio/dados_3.js
@@ -0,0 +1,14 @@
+//Tipos de dados
+let idade = 31; 
+console.log(typeof 31) ; // retorna o tipo da varivel
+console.log(typeof idade); // retorna um number
+
+let salario = 4568.32; 
+
+console.log(typeof salario); // retorna um number
+
+ let estaChovendo = false; // Aceita true or false
+
+ console.log(typeof estaChovendo); // tipo Boolean
+ console.log(typeof "teste"); //string : sequencia de caracteres
+ console.log(typeof "1"); // Retorna uma string
