116417c691f74dc095b820573eef8ea1ef416ae5
ProfNando <mestre.physics@gmail.com>
2022-06-19 20:07:02 -0300

    dados_4

---
+++ Inicio/dados_4.js
@@ -0,0 +1,15 @@
+
+let a = 3;
+
+
+        //...
+
+a = 10;
+
+
+
+console.log(a); 
+
+const b = 4; // E mais interessantes ter mais constantes do que variaveis no programa.
+b = 5; // NAO ALTERA valores de CONSTANTE!!!
+console.log(b);
