e3cca5858cd5a2e0c12e5e5e846e97bfd4a658a3
ProfNando <mestre.physics@gmail.com>
2022-06-19 18:53:34 -0300

    dados_1

---
+++ Inicio/dados_1.js
@@ -0,0 +1,15 @@
+var nome = "Caneta";
+var quantidade = 10; //Var (variavel) Armazena uma informação
+var preco = 6.4;
+var imposto = 1.5;
+var valor_final = preco + imposto;
+
+
+console.log(nome);
+console.log(quantidade); // Valor literal
+console.log(preco);
+console.log(imposto);
+console.log(valor_final);
+
+nome = "Caneta BIC";
+console.log(nome); 
