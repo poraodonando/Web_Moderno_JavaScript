10183cb759113208c037263edafbb3b6cc3dea37
ProfNando <mestre.physics@gmail.com>
2022-06-19 20:33:17 -0300

    desafio troca

---
+++ Inicio/desafio_troca.js
@@ -0,0 +1,13 @@
+ // Desafio da troca instanciar dois valores e trocar seus valores
+
+ let a = 10;
+ let b = 20;
+ let aux;
+
+ aux = a;
+ a = b;
+ b = aux;  
+
+
+ console.log("O novo valor de A: " + a);
+ console.log("O novo valor de B: " + b);
