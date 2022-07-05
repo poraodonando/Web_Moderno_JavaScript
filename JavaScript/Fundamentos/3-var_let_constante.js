e4777e5a8875fa6e8c0b2256fc2edcb94a121152
ProfNando <mestre.physics@gmail.com>
2022-07-05 10:48:14 -0300

    var e let

---
+++ JavaScript/Fundamentos/3-var_let_constante.js
@@ -0,0 +1,28 @@
+var num_a = 4;
+/**
+ * var -> declara
+ * num_a -> identificador (nomeia as coisas)
+ * = -> simbolo de ATRIBUIÇÃO
+ * 4 -> valor
+ * 
+ */
+
+let num_a = 5;
+//Crie variaveis usando let, é mais moderno usar let
+
+var num_b = 30;
+// let num_b = 20; nao posso redeclarar uma variavel usando let
+num_b = 30;
+
+console.log(num_a, num_b);
+
+num_a = 321;
+num_b = 654;
+
+const c = 50;
+
+// c = 40;
+// por ser constante nao posso atribuir novo valor
+// Quanto mais constante tiver no programa, melhor
+
+console.log(c);
