fdcd92491c92c38af93a53c59a5f7af8c69d0cd6
ProfNando <mestre.physics@gmail.com>
2022-07-05 10:57:19 -0300

    up

---
+++ JavaScript/Fundamentos/4-tipagem_fraca.js
@@ -0,0 +1,17 @@
+let qualquer = 'legal';
+
+console.log(qualquer);
+console.log(typeof qualquer)// diz o tipo de variavel
+
+qualquer = 3.1516;
+console.log(typeof qualquer);
+
+// uma mesma variavel pode ter multiplos valores o que significa que javaScript tem uma tipagem fraca
+
+qualquer = false;
+console.log(typeof qualquer);
+
+//Evite escolher nomes genericos e siglas para as variaveis
+let valor = '';
+let numero = 1;
+let pqp = false // Produto quimico perigoso ...  kkk
