bada67e729f2c6dbb29ae5196a333cb4ccaf600e
ProfNando <mestre.physics@gmail.com>
2022-07-05 14:20:27 -0300

    up

---
+++ JavaScript/Fundamentos/numeros_alguns_cuidados.js
@@ -0,0 +1,18 @@
+console.log(7/0.1); // 70
+console.log(7/0.01); // 700
+console.log(7/0.001); // 7000
+
+
+console.log(7/0.00000000000000001);
+
+console.log(7/0); //Infinity
+
+
+console.log('10'/2); // 5
+console.log('10.3'/2);
+console.log('Show!'*2); //NaN
+console.log(0.1 + 0.7) // 0.799999
+//console.log(10.toString())
+console.log(10.345.toFixed(2)); // deixa o numero com 2 casas decimais
+
+
