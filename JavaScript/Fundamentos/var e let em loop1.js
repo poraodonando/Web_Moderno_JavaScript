d97221ab414f7ff3a5f8c7a119fbdb56d5d533aa
ProfNando <mestre.physics@gmail.com>
2022-07-11 14:32:42 -0300

    up

---
+++ JavaScript/Fundamentos/var e let em loop1.js
@@ -0,0 +1,17 @@
+// Para VAR
+
+for(var i = 0; i < 10 ; i++){
+    console.log(i);
+}
+
+console.log("i = ", i); 
+
+// Para LET
+
+
+for(let r = 0; r < 10; r++){
+    console.log(r); // a variavel let r só está disponivel no bloco.
+}
+
+console.log("r = ",r); //DA ERRO!
+
