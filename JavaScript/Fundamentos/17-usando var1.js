6af4605363dc413cc37c2db95282566767f0286c
ProfNando <mestre.physics@gmail.com>
2022-07-11 13:26:11 -0300

    up

---
+++ JavaScript/Fundamentos/17-usando var1.js
@@ -0,0 +1,18 @@
+{
+  {
+    {
+      {var sera = 'Será ???'}
+      console.log(sera);
+    }
+  }
+}
+
+console.log(sera);
+
+function teste1(){
+    var local = 123;
+}
+
+teste1();
+console.log(local); //Nao conseguimos acessar a variável
+
