a7018c2e49bf2730423cf52ea64e791b986d377a
ProfNando <mestre.physics@gmail.com>
2022-07-12 15:25:30 -0300

    função VS objeto

---
+++ JavaScript/Fundamentos/22-funcaoVSobjeto.js
@@ -0,0 +1,10 @@
+console.log(typeof Object) // função
+console.log(typeof new Object) // chamando um construtor (instanciando uma )
+const cliente = function(){}
+console.log(typeof cliente);
+console.log(typeof new cliente);
+
+class produto{}; // ES 2015 (ES6)
+console.log(typeof produto);//
+console.log(typeof new produto());
+
