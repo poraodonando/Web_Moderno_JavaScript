77157c27e0b01fa64c709875b376420fab3c0145
ProfNando <mestre.physics@gmail.com>
2022-07-12 20:16:42 -0300

    up

---
+++ JavaScript/Fundamentos/25-operadores_de_atribuição.js
@@ -0,0 +1,11 @@
+const a = 7; // o sinal de igual '=' atribui e nao compara
+let b = 3; 
+b += a; // b = b + a (atribuição aditiva)
+console.log(b);
+b -= a; // b = b - a (atribuição subtrativa)
+console.log(b);
+b *= a; // b = b * a (atribuição multiplcativa)
+console.log(b);
+b /= a; // b = b / a (atribuição divsiva)
+console.log(b);
+b %= a; // b = b % a (atribuição modular)
