da56b1e10a55c75412627cd258d9b579431d4766
ProfNando <mestre.physics@gmail.com>
2022-07-13 13:43:40 -0300

    up

---
+++ JavaScript/Fundamentos/30-operadores_aritmeticos.js
@@ -0,0 +1,20 @@
+// São binarios, operam acima de dois operandos
+const[a, b, c, d] = [3, 5, 1, 15];
+const soma = a + b + c + d; // Operação binária
+const subtracao = d - b;
+const multiplicacao = a * c;
+const divisao = b / c;
+const modulo = d % 2
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+
+
+
+//  op1 + op2 => Sintaxe In-Fixed
+// d++ => Sintaxe Post-Fixed
+// ++d => Sintaxe Pre-Fixed
+// -a => Operador unário
