b95d5a125533f4ffae6a3e918e5f9f447a9288c3
ProfNando <mestre.physics@gmail.com>
2022-07-15 10:11:39 -0300

    up

---
+++ JavaScript/Fundamentos/35-tratamento_de_erro.js
@@ -0,0 +1,25 @@
+function tratarErroELAncar(erro){
+    //throw new Error('...')
+    //throw 10;
+    //throw true
+    throw{
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+
+function imprimirNomeGritado(obj) {
+    try{
+        console.log(obj.name.toUpperCase()+'!!!!');    
+    }catch(e){
+        tratarErroELAncar(e)
+    } finally {
+        console.log('Final');
+    }
+    
+}
+
+const obj ={nome: 'Roberto'}
+imprimirNomeGritado(obj)
