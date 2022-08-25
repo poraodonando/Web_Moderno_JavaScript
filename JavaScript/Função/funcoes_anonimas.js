7eda4fcee6a21c933d4bf2ea7158f3dbaa3fdab1
ProfNando <mestre.physics@gmail.com>
2022-08-25 15:58:14 -0300

    up

---
+++ JavaScript/Função/funcoes_anonimas.js
@@ -0,0 +1,26 @@
+// Função anônima é uma função sem nome
+
+const soma = function(x, y ){ 
+       return x + y;
+}
+
+const imprimirResultado = function(a, b, operacao = soma){
+    console.log(operacao(a, b));
+}
+
+imprimirResultado(3,4);
+imprimirResultado(2,6, soma);
+
+imprimirResultado(5,7, function(x, y ){
+    return x - y;
+})
+
+imprimirResultado(8,2, (x, y) => x *y)
+
+const pessoa = {
+    falar: function(){
+        console.log('Opa');
+    }
+}
+
+pessoa.falar();
