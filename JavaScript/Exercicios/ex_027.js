95df71ecc4ffb4ccdf405d6284fdbcbad413ecaa
ProfNando <mestre.physics@gmail.com>
2022-09-02 13:15:58 -0300

    up

---
+++ JavaScript/Exercicios/ex_027.js
@@ -0,0 +1,39 @@
+ /**
+  * Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais
+  * de duas crianças e calcule se existe uma criança menor, caso exista se a criança menor 
+  * ultrapassará a maior e em quantos anos isso acontecerá. Utilize centímetros para as 
+  * unidades de medida.
+  */
+
+ function crescAnual(alt_a, tax_a, alt_b, tax_b){
+
+    let tam_a = alt_a * tax_a;
+    let tam_b = alt_b * tax_b;
+    let maior,menor, ano = 0;
+
+        if(alt_a != alt_b){
+
+            if(alt_a > alt_b){
+                maior = tam_a;
+                menor = tam_b
+
+            }else{
+                maior = tam_b;
+                menor = tam_a;
+            }
+
+            for(let i = menor; i <= maior;i++ ){
+                ano++;
+            }
+        }else{
+            return console.log('Ambos tem a mesma altura');
+        }
+
+        return console.log(`Eles terão tamanho iguais em ${ano} anos`);
+
+
+ }
+
+
+
+ crescAnual(123, 0.2, 165, 0.2);
