2c1fd03c0eff86c76c7c578b39eb33e550c119c3
ProfNando <mestre.physics@gmail.com>
2022-09-02 17:45:11 -0300

    up

---
+++ JavaScript/Exercicios/ex_034.js
@@ -0,0 +1,20 @@
+/**
+ * Construa uma função que receberá duas Strings de tamanhos variados e que retornará 
+ * True ou False caso todos os caracteres (independentemente de ser maiúsculo ou minúsculo)
+ * estejam contidos em ambas palavras.
+ */
+
+
+function contido(str_a, str_b){
+
+        for(let i = 0; i < str_a.legnth; i++){
+
+            for(let j = 0; j < str_b.legnth; j++){
+
+                
+
+            }
+
+        }
+
+}
