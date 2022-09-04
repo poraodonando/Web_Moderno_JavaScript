08dc596795239000b79ae9050d40c36e44f545ae
ProfNando <mestre.physics@gmail.com>
2022-09-04 19:26:23 -0300

    up

---
+++ JavaScript/Objeto/getter_setter.js
@@ -0,0 +1,19 @@
+const sequencia = {
+    _valor: 1, //colocar '_' Convenção que é pretendida  ser acessada apenas internamente
+    get valor(){
+        return this._valor++;
+    },
+    
+    set valor(valor){
+        
+        if(valor > this._valor){
+            this._valor = valor     
+        }        
+    }    
+}
+
+console.log(sequencia.valor,sequencia.valor);
+sequencia.valor = 1000;
+console.log(sequencia.valor, sequencia.valor);
+sequencia.valor = 900;
+console.log(sequencia.valor, sequencia.valor);
