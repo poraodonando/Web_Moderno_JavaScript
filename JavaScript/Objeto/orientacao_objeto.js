46df30004109a490d25a1ca872f905b9c518a7b0
ProfNando <mestre.physics@gmail.com>
2022-09-04 11:34:00 -0300

    up

---
+++ JavaScript/Objeto/orientacao_objeto.js
@@ -0,0 +1,40 @@
+//CODIGO NAO EXECUTÁVEL
+
+// Procedural
+
+processamento(valor1, valor2, valor3);
+
+// OO
+
+objeto = {
+    valor1, 
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento(); // Foco passou a ser o objeto
+
+//Principios Importantes
+
+/**
+ * 1º ABSTRAÇÃO - > Pegar um objeto do mundo real e
+ * como traduzir para o seu sistema, abstraindo seus
+ * métodos, atributos e comportamento.
+ * 
+ * 2º ENCAPSULAMENTO - > Ter os detalhes de 
+ * implementação escondidos e mostrar uma interface
+ * simples para que voce possa interagir com 
+ * determinado objeto.  
+ * 
+ * 3º HERANÇA -> O objeto herda caracteristicas de um
+ * outros objetos, recebendo métodos, atributos e
+ * comportamentos a partir de outro objeto.
+ * 
+ * 4º POLIMORFISMO -> O objeto pode possuir multiplas
+ * formas, partindo de uma variável mais genérica.
+ * 
+ */
+
