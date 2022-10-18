13540a57add450f04cfab29ce3ac32d4c408d13a
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-18 15:42:58 -0300

    node

---
+++ NODE/moduloA.js
@@ -0,0 +1,11 @@
+// Um arquivo em NODE representa um múdulo.
+//Módulo nao deixa disponivel o que ta escrito nele para todo mundo.
+
+
+//Formas de exportar um arquivo NODE
+
+this.ola = 'Fala Pessoal!'; // Objeto
+
+exports.bemVindo = 'Bem vindo ao Node';
+
+module.exports.ateLogo = 'Até a proxima';
---
+++ NODE/moduloB.js
@@ -0,0 +1,8 @@
+//Exportação de forma mais cássica
+module.exports = {
+    bomDia: 'Bom dia!',
+    boaNoite(){
+        return 'Boa Noite'
+    }
+
+}
