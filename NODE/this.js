a44ff9d45ba3be5b0799a8d1849ff2f014b0ca23
Prof. Nando Batista <webdev.physics@gmail.com>
2022-11-19 11:39:23 -0300

    Update this.js

--- NODE/this.js
+++ NODE/this.js
@@ -8,6 +8,7 @@ function logThis(){
     console.log('Dentro de uma função...')
     console.log(this ==exports)
     console.log(this === module.exports)
+    console.log(this === global)
 }
 
 logThis()
